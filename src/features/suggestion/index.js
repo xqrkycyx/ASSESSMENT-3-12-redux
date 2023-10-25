1ff8f519ba3a155527a3f78e9dbfefea270c2bea
Will Williams <wwilliams@indeed.com>
2023-10-24 22:13:07 -0700

    Requirement 4: Load a photo suggestion

--- src/features/suggestion/index.js
+++ src/features/suggestion/index.js
@@ -5,12 +5,14 @@ import {
   selectError,
   selectLoading,
   // Task 18: Import the `selectSuggestion()` selector from the suggestion slice
+  selectSuggestion,
 } from "./suggestion.slice";
 import "./suggestion.css";
 
 export default function Suggestion() {
   // Task 19: Call useSelector() with the selectSuggestion() selector
   // The component needs to access the `imageUrl` and `caption` properties of the suggestion object.
+  const { caption, imageUrl } = useSelector(selectSuggestion);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
   const dispatch = useDispatch();
@@ -18,6 +20,7 @@ export default function Suggestion() {
   useEffect(() => {
     async function loadSuggestion() {
       // Task 20: Dispatch the fetchSuggestion() action creator
+      await dispatch(fetchSuggestion());
     }
     loadSuggestion();
   }, [dispatch]);
@@ -31,8 +34,8 @@ export default function Suggestion() {
     // Task 21: Enable the two JSX lines below needed to display the suggestion on the page
     render = (
       <>
-        {/* <img alt={caption} src={imageUrl} />
-        <p>{imageUrl}</p> */}
+        <img alt={caption} src={imageUrl} />
+        <p>{imageUrl}</p>
       </>
     );
   }
--- src/features/suggestion/suggestion.slice.js
+++ src/features/suggestion/suggestion.slice.js
@@ -1,7 +1,14 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchSuggestion =
-  createAsyncThunk(/* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */);
+export const fetchSuggestion = createAsyncThunk(
+  /* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */
+  "suggestion/fetch",
+  async () => {
+    const response = await fetch("http://localhost:3004/api/suggestion");
+    const data = await response.json();
+    return data.data;
+  }
+);
 
 const initialState = {
   suggestion: "",
@@ -13,8 +20,21 @@ const options = {
   name: "suggestion",
   initialState,
   reducers: {},
-  extraReducers: {
+  extraReducers: (builder) => {
     /* Task 16: Inside `extraReducers`, add reducers to handle all three promise lifecycle states - pending, fulfilled, and rejected - for the `fetchSuggestion()` call */
+    builder
+      .addCase(fetchSuggestion.pending, (state) => {
+        state.loading = true;
+        state.error = null; // Reset the error state
+      })
+      .addCase(fetchSuggestion.fulfilled, (state, action) => {
+        state.loading = false;
+        state.suggestion = action.payload;
+      })
+      .addCase(fetchSuggestion.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
   },
 };
 
@@ -23,6 +43,7 @@ const suggestionSlice = createSlice(options);
 export default suggestionSlice.reducer;
 
 // Task 17: Create a selector, called `selectSuggestion`, for the `suggestion` state variable and export it from the file
+export const selectSuggestion = (state) => state.suggestion.suggestion;
 
 export const selectLoading = (state) => state.suggestion.loading;
 export const selectError = (state) => state.suggestion.error;
